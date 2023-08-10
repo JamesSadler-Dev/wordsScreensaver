7a93c70370ead6a58328b0b3fed07cba4e084395
James <105584897+James-Sorrow@users.noreply.github.com>
2023-08-10 15:28:18 -0500

    Add a color to welcome animation

--- screensaverUtil.hh
+++ screensaverUtil.hh
@@ -480,22 +480,28 @@ class wordScreensaver{
             {
                 system("cls");
                 printBuffer(9);
-                std::cout << "\t\u001b[34m\u001b[4m" << "Welcome";
+                std::cout << "\t\u001b[35m\u001b[4m" << "Welcome";
                 std::cout << "\u001b[0m";
                 printBuffer(3);
                 this_thread::sleep_for(chrono::milliseconds(70));
+                system("cls");
+                printBuffer(9);
+                std::cout << "\t\u001b[34m\u001b[4m" << "Welcome";
+                std::cout << "\u001b[0m";
+                printBuffer(3);
+                this_thread::sleep_for(chrono::milliseconds(65));
                 system("cls");
                 printBuffer(9);
                 std::cout << "\t\u001b[34;1m\u001b[4m" << "Welcome";
                 std::cout << "\u001b[0m";
                 printBuffer(3);
-                this_thread::sleep_for(chrono::milliseconds(70));
+                this_thread::sleep_for(chrono::milliseconds(65));
                 system("cls");
                 printBuffer(9);
                 std::cout << "\t\u001b[0m\u001b[4m" << "Welcome";
                 std::cout << "\u001b[0m";
                 printBuffer(3);
-                this_thread::sleep_for(chrono::milliseconds(120));
+                this_thread::sleep_for(chrono::milliseconds(140));
                 return 0;
             }
         }
