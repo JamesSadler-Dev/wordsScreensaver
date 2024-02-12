46b255e966fe16b69b3c00713718b94da6642fb6
James Sadler <jamesironmanjames@gmail.com>
2024-02-11 19:09:16 -0600

    move constants into public block and ignore .exe files

--- screensaverUtil.hh
+++ screensaverUtil.hh
@@ -13,27 +13,7 @@ using vectors = std::vector<std::string>;
 using cstring = const char *;
 
 
-const cstring COL1 = "\u001b[35m";
-const cstring COL2 = "\u001b[32m";
-const cstring COL3 = "\u001b[34m";
-const cstring COL4 = "\u001b[97m";
-const cstring COL5 = "\u001b[31m";
-const cstring COL6 = "\u001b[95m";
-const cstring COL7 = "\u001b[33m";
-// lengths to set conditions with
-const int WRAP_LEN = 18;
-const int LENGTH_LIMIT = 44;
-const int FIRSTP_LIMIT = 22;
-const int PAD_XXXL = 20;
-const int PAD_XXL = 19;
-const int PAD_XL = 18;
-const int PAD_L = 17;
-const int PAD_M = 15;
-const int PAD_S = 13;
-const int PAD_XS = 11;
-const int PAD_XXS = 10;
-const int PAD_XXXS = 9;
-const int PAD_XXXXS = 6;
+
 
 
 class wordScreensaver{
@@ -426,6 +406,29 @@ private:
     }
 
 public:
+
+    const cstring COL1 = "\u001b[35m";
+    const cstring COL2 = "\u001b[32m";
+    const cstring COL3 = "\u001b[34m";
+    const cstring COL4 = "\u001b[97m";
+    const cstring COL5 = "\u001b[31m";
+    const cstring COL6 = "\u001b[95m";
+    const cstring COL7 = "\u001b[33m";
+    // lengths to set conditions with
+    const int WRAP_LEN = 18;
+    const int LENGTH_LIMIT = 44;
+    const int FIRSTP_LIMIT = 22;
+    const int PAD_XXXL = 20;
+    const int PAD_XXL = 19;
+    const int PAD_XL = 18;
+    const int PAD_L = 17;
+    const int PAD_M = 15;
+    const int PAD_S = 13;
+    const int PAD_XS = 11;
+    const int PAD_XXS = 10;
+    const int PAD_XXXS = 9;
+    const int PAD_XXXXS = 6;
+    
     wordScreensaver(int fontSize, cstring filename)
     {
         setFilename(filename);
