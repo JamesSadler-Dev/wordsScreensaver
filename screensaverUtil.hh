83b8172185ccc82fcaf41037ed8aba16102c4f6d
JamesSadler-Dev <jamesironmanjames@gmail.com>
2024-02-12 22:38:20 -0600

    fix style in screensaverutil.hh

--- screensaverUtil.hh
+++ screensaverUtil.hh
@@ -31,9 +31,7 @@ private:
     {
         file.open(filename);
         while (std::getline(file, word))
-        {
             words.push_back(word);
-        }
     }
 
     void setfontsize(int fontsize)
@@ -102,41 +100,29 @@ private:
             that should've been stripped with the textformatter.py
         */
         if (current[0] == ' ' || current[0] == '\t' || current[0] == '\r')
-        {
             ; // empty block on purpose
-        }
 
         /*
          *   CASE 1: LINE IS REALLY LONG JUST PRINT NORMALLY
          */
 
         else if (totalLen > LENGTH_LIMIT)
-        {
             sprintf(outputWord, "%s%s", COL2, totalWordArr);
-        }
         else if (totalLen > WRAP_LEN)
         {
-
             int indexOfSpace = current.find_last_of(" ");
-
-            /* CASE 2:
-             *    IF LONGER THAN WRAP_LEN BUT NO SPACES
+            /* 
+            CASE 2: IF LONGER THAN WRAP_LEN BUT NO SPACES
              */
             if (indexOfSpace == -1)
             {
                 // extra padding if it's too long
                 if (totalLen > PAD_XXL)
-                {
                     sprintf(outputWord, "%s%23s", COL1, totalWordArr);
-                }
                 else if (totalLen > PAD_M)
-                {
                     sprintf(outputWord, "%s%21s", COL1, totalWordArr);
-                }
                 else
-                {
                     sprintf(outputWord, "%s%17s", COL2, totalWordArr);
-                }
             }
             /* CASE 3:
              *    IF FIRSTPART IS LONGER THAN LONG CUTOFF
@@ -161,67 +147,43 @@ private:
                     if (lpLength < PAD_XXXXS)
                     {
                         if (fpLength > PAD_XXL)
-                        {
                             sprintf(outputWord, "%s%22s\n%14s", COL2, p1Array, p2Array);
-                        }
                         else
-                        {
                             sprintf(outputWord, "%s%20s\n%14s", COL1, p1Array, p2Array);
-                        }
-
                         // FIRSTPART LONGER THAN LONG CUTOFF AND SHORTER THAN XLCUTOFF -- NORMAL LASTPART
                     }
                     else if (fpLength < PAD_L)
                     {
                         // DON'T PAD LASTPART AS MUCH IF IT's TOO SHORT
                         if (lpLength < PAD_XXXS)
-                        {
                             sprintf(outputWord, "%s%19s\n%16s", COL2, p1Array, p2Array);
-                        }
                         else
-                        {
                             sprintf(outputWord, "%s%19s\n%18s", COL1, p1Array, p2Array);
-                        }
                     }
                     else
                     {
                         // FIRSTPART EVEN LONGER THAN XL CUTOFF
                         //  if FIRSTPART too long just format it normally
                         if (fpLength > FIRSTP_LIMIT)
-                        {
                             sprintf(outputWord, "%s%s", COL2, totalWordArr);
                             // long first part and long last part
-                        }
                         else if (fpLength > PAD_XXXL)
                         {
                             if (lpLength > PAD_L)
-                            {
                                 sprintf(outputWord, "%s%22s\n%21s", COL2, p1Array, p2Array);
-                            }
                             else
-                            {
                                 sprintf(outputWord, "%s%22s\n%18s", COL2, p1Array, p2Array);
-                            }
                             // long firstpart and lastpart
                         }
                         else if (lpLength > PAD_XL)
-                        {
                             sprintf(outputWord, "%s%21s\n%22s", COL2, p1Array, p2Array);
-
                             // lastpart too short print normally
-                        }
                         else if (lpLength < PAD_XXXXS)
-                        {
                             sprintf(outputWord, "%s%s", COL2, totalWordArr);
-                        }
                         else if (lpLength < PAD_XXXS)
-                        {
                             sprintf(outputWord, "%s%21s\n%16s", COL2, p1Array, p2Array);
-                        }
                         else
-                        {
                             sprintf(outputWord, "%s%22s\n%21s", COL2, p1Array, p2Array);
-                        }
                     }
 
                     /* CASE 4:
@@ -231,19 +193,12 @@ private:
                 else if (fpLength > PAD_S)
                 {
                     if (lpLength < PAD_XXXXS)
-                    {
                         sprintf(outputWord, "%s%19s\n%14s", COL1, p1Array, p2Array);
                         // DON'T PAD LASTPART AS MUCH IF IT's TOO SHORT
-                    }
                     else if (lpLength < PAD_XXS)
-                    {
                         sprintf(outputWord, "%s%18s\n%15s", COL2, p1Array, p2Array);
-                    }
                     else
-                    {
                         sprintf(outputWord, "%s%19s\n%17s", COL2, p1Array, p2Array);
-                    }
-
                     /* CASE 5:
                      *
                      *    IF FIRSTPART IS SHORTER THAN PAD LONG OR SHORT CUTOFF
@@ -252,79 +207,47 @@ private:
                 }
                 else if (fpLength < PAD_S)
                 {
-
                     if (fpLength == lpLength)
-                    {
                         sprintf(outputWord, "%s%16s\n%16s", COL1, p1Array, p2Array);
-                    }
                     else if (fpLength <= PAD_XXXXS)
                     {
                         if (lpLength > PAD_XXL)
-                        {
                             sprintf(outputWord, "%s%14s\n%23s", COL1, p1Array, p2Array);
-                        }
                         else if (lpLength > PAD_M)
-                        {
                             sprintf(outputWord, "%s%14s\n%19s", COL1, p1Array, p2Array);
-                        }
                         else
-                        {
                             sprintf(outputWord, "%s%14s\n%19s", COL1, p1Array, p2Array);
-                        }
-
                         // if firstpart between padleftTiny-padLeftShort
                     }
                     else if (fpLength < PAD_XXXS)
-                    {
                         sprintf(outputWord, "%s%15s\n%17s", COL2, p1Array, p2Array);
-
                         // If LASTPART is LESS than tiny or medium cutoff
-                    }
                     else if (lpLength < PAD_XXXXS)
-                    {
                         sprintf(outputWord, "%s%16s\n%13s", COL2, p1Array, p2Array);
-
                         // DON'T PAD LASTPART AS MUCH IF IT's TOO SHORT
-                    }
                     else if (lpLength < PAD_XXXS)
-                    {
                         sprintf(outputWord, "%s%16s\n%14s", COL2, p1Array, p2Array);
-                    }
                     else if (fpLength >= PAD_XS)
                     {
                         if (lpLength > PAD_L)
-                        {
                             sprintf(outputWord, "%s%17s\n%20s", COL2, p1Array, p2Array);
-                        }
                         else
-                        {
                             sprintf(outputWord, "%s%17s\n%16s", COL2, p1Array, p2Array);
-                        }
                     }
                     else if (fpLength >= PAD_XXXS)
                     {
                         if (lpLength > PAD_L)
-                        {
                             sprintf(outputWord, "%s%16s\n%23s", COL2, p1Array, p2Array);
-                        }
                         else if (lpLength >= PAD_XS)
-                        {
                             sprintf(outputWord, "%s%16s\n%17s", COL2, p1Array, p2Array);
-                        }
                         else
-                        {
                             sprintf(outputWord, "%s%16s\n%16s", COL2, p1Array, p2Array);
-                        }
                         // if LASTPART is LONGER than padding cutoff
                     }
                     else if (lpLength > PAD_S)
-                    {
                         sprintf(outputWord, "%s%16s\n%21s", COL2, p1Array, p2Array);
-                    }
                     else
-                    {
                         sprintf(outputWord, "%s%17s\n%18s", COL2, p1Array, p2Array);
-                    }
                 }
             }
         }
@@ -337,29 +260,17 @@ private:
         else
         {
             if (totalLen > PAD_L)
-            {
                 sprintf(outputWord, "%s%21s", COL3, totalWordArr);
-            }
             else if (totalLen > PAD_M)
-            {
                 sprintf(outputWord, "%s%20s", COL3, totalWordArr);
-            }
             else if (totalLen > PAD_S)
-            {
                 sprintf(outputWord, "%s%19s", COL5, totalWordArr);
-            }
             else if (totalLen > PAD_XXS)
-            {
                 sprintf(outputWord, "%s%17s", COL6, totalWordArr);
-            }
             else if (totalLen < PAD_XXXXS)
-            {
                 sprintf(outputWord, "%s%13s", COL7, totalWordArr);
-            }
             else
-            {
                 sprintf(outputWord, "%s%15s", COL4, totalWordArr);
-            }
         }
     }
 
@@ -370,26 +281,16 @@ private:
         {
             // check for escape key or clicking
             if (GetAsyncKeyState(VK_ESCAPE))
-            {
                 return 1;
-            }
             else if (GetAsyncKeyState(VK_LBUTTON))
-            {
                 return 1;
-            }
             else if (GetAsyncKeyState(VK_RBUTTON))
-            {
                 return 1;
-            }
             else if (GetAsyncKeyState(VK_SPACE))
-            {
                 return 1;
-            }
             // Skip this word but don't exit program with kb right function
             else if (GetAsyncKeyState(VK_RIGHT))
-            {
                 return 0;
-            }
             // sleep for one second n times in the loop to be more responsive
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
@@ -400,9 +301,7 @@ private:
     void printBuffer(int n)
     {
         for (int i = 0; i < n; i++)
-        {
             std::cout << "\n";
-        }
     }
 
 public:
@@ -479,18 +378,12 @@ public:
             if (!past.empty() || past == "")
             {
                 if (current == past)
-                {
                     continue;
-                }
                 else
-                {
                     past = current;
-                }
             }
             else
-            {
                 past = current;
-            }
 
             char outputWord[100];
             setSplitLogic(current, outputWord);
@@ -498,9 +391,7 @@ public:
             // guardian pattern
 
             if (current.length() < 2 || current == "" || outputWord[0] == ' ' || outputWord[1] == ' ')
-            {
                 continue;
-            }
 
             system("cls");
             std::this_thread::sleep_for(std::chrono::milliseconds(1));
